d9ded9698c96c7a24eda1c313753b30527834c19
ajinkyachirde07 <ajinkyachirde07@gmail.com>
2025-09-08 03:26:57 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000","https://bid-bridge-auction-web-app.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true
   }
